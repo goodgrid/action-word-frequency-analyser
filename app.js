359587437d79beeb911ff1d0585c3e4321e05a7f
Koen Bonnet <koen@goodgrid.nl>
2021-10-19 21:32:47 +0200

    implemented processing of csv columns

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import fs from "fs";
-import csv from "csv-parser";
+import neatCsv from "neat-csv";
 
 let argType = process.argv[2]
 let argFile = process.argv[3]
 let argColl = process.argv[4]
 
 
-let delimiters = /[\s_]/
+let delimiters = /[\s_-]/
 
 let filters = [
     /\n/g,
@@ -17,39 +17,36 @@ let filters = [
     /,/g,
 ]
 
-
-
 fs.promises.readFile(argFile).then(buffer => {
     let content = buffer.toString();
-    if (argType=="csv")  content = getColumnContent(buffer,argColl);
+    if (argType=="csv")  content = getColumnContent(content,argColl);
+    if (argType=="txt")  content = getPlainTextContent(content);
 
-    /*
-    for (let filter of filters) {
-        content = content.replace(filter," ")
-    }
-    content = content.replace(/ +(?= )/g,''); // replace all multiple spaces by a single space
-    
-    let contentArray = content.split(delimiters);
-    
-
-    let wordCount = contentArray.map(word => {
-        return {
-            word: word.toUpperCase(),
-            count: contentArray.filter(countWord => countWord.toUpperCase() == word.toUpperCase()).length
+    content.then(content => {
+        for (let filter of filters) {
+            content = content.replace(filter," ")
         }
+        content = content.replace(/ +(?= )/g,''); // replace all multiple spaces by a single space
+        
+        let contentArray = content.split(delimiters);
+        
+    
+        let wordCount = contentArray.map(word => {
+            return {
+                word: word.toUpperCase(),
+                count: contentArray.filter(countWord => countWord.toUpperCase() == word.toUpperCase()).length
+            }
+        })
+        wordCount = dedup(wordCount);
+        wordCount = wordCount.sort(sortObjects)
+    
+        console.log(dedup(wordCount))
     })
-    wordCount = dedup(wordCount);
-    wordCount = wordCount.sort(sortObjects)
-
-
-    console.log(dedup(wordCount))
-    */
 })
 
 
 function sortObjects(word1, word2) {
     return word2.count - word1.count
-
 }
 
 function dedup(arr) {
@@ -63,13 +60,19 @@ function dedup(arr) {
 	});
 }
 
-function getColumnContent(buffer) {
-    csv(buffer)
-    .on('data', (data) => {
-        console.log(data)
-        results.push(data)
-    })
-    .on('end', () => {
-        console.log(results);
-    });
+async function getPlainTextContent(content) {
+    /*
+        This is a dummy function only to have a promise returned, just like 
+        when a CSV string is processed, so we can keep the rest generic
+    */
+    return content
+}
+
+async function getColumnContent(content, column) {
+    let csv = await neatCsv(content,{separator:";"})
+
+    csv = csv.map(line => line[column]);
+
+    return csv.join(" ");
+    
 }
