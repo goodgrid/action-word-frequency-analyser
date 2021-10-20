bb295c37a23c13c4f1b7d7a608fa33a0df21003b
Koen Bonnet <koen@goodgrid.nl>
2021-10-20 20:54:56 +0200

    Split filtering of characters from words and language specific stop words. Also implemented some progress indication. Large data sets take up to 6 hours so far

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ import neatCsv from "neat-csv";
 let argType = process.argv[2]
 let argFile = process.argv[3]
 let argColl = process.argv[4]
+let argLang = process.argv[5]
 
 
 let delimiters = /[\s_-]/
 
-let filters = [
+let characterFilters = [
     /\n/g,
     /\r/g,
     /\"/g,
@@ -17,33 +18,117 @@ let filters = [
     /,/g,
 ]
 
+let wordFilters = [
+    /PDF/i,
+    /MSG/i,
+    /XLSX/i,
+    /JPG/i,
+    /DOCX/i,
+    /DOC/i,
+    /JFIF/i,
+    /ZIP/i,
+    /[0-9]+/,
+    /re/i,
+    /aw/i,
+    /fw/i,
+
+    //
+]
+
+let stopWords = {
+    "French": [
+        /au/i,
+        /de/i,
+        /du/i
+    ],
+    "German": [
+        /und/i,
+        /zum/i,
+        /ab/i,
+        /der/i,
+    ],
+    "Dutch": [
+        /van/i,
+        /de/i,
+        /en/i,
+        /naar/i
+    ]
+
+}
+
 fs.promises.readFile(argFile).then(buffer => {
     let content = buffer.toString();
     if (argType=="csv")  content = getColumnContent(content,argColl);
     if (argType=="txt")  content = getPlainTextContent(content);
 
     content.then(content => {
-        for (let filter of filters) {
+        console.log(new Date(),"Content read")
+        for (let filter of characterFilters) {
             content = content.replace(filter," ")
         }
         content = content.replace(/ +(?= )/g,''); // replace all multiple spaces by a single space
-        
+        console.log(new Date(),"Characters filtered")
+
         let contentArray = content.split(delimiters);
+        console.log(new Date(),`Content split into ${contentArray.length} words`)
         
     
-        let wordCount = contentArray.map(word => {
+        contentArray = contentArray.filter(word => {
+            if (!isExcludedWord(word)) {
+                return word
+            }
+        })
+        console.log(new Date(),`Words filtered into ${contentArray.length} words`)
+
+
+        contentArray = contentArray.filter(word => {
+            if (!isStopWord(word,argLang)) {
+                return word
+            }
+        })
+        console.log(new Date(),`Language specific stop words filtered into ${contentArray.length} words`)
+
+
+        let wordCount = contentArray.map((word,index) => {
+            process.stdout.clearLine(); 
+            process.stdout.cursorTo(0); 
+            process.stdout.write(`Counting word ${index.toString().padStart((contentArray.length).toString().length,"0")} of ${contentArray.length}`)
             return {
                 word: word.toUpperCase(),
                 count: contentArray.filter(countWord => countWord.toUpperCase() == word.toUpperCase()).length
             }
         })
+        process.stdout.clearLine(); 
+        process.stdout.cursorTo(0); 
+        console.log(new Date(),`Counted frequency`)
+
         wordCount = dedup(wordCount);
+        console.log(new Date(),"Words deduplicated")
+
         wordCount = wordCount.sort(sortObjects)
-    
-        console.log(dedup(wordCount))
+        console.log(new Date(),"Words sorted on frequency")
+
+        console.log(wordCount.slice(0,50))
     })
 })
 
+function isExcludedWord(word) {
+    for (let filter of wordFilters) {
+        if (filter.test(word.toUpperCase())) {
+            return true
+        }
+    }
+    return false
+}
+
+function isStopWord(word, language) {
+    for (let filter of stopWords[language]) {
+        if (filter.test(word.toUpperCase())) {
+            return true
+        }
+    }
+    return false
+}
 
 function sortObjects(word1, word2) {
     return word2.count - word1.count
